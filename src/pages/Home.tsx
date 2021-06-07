eee760d9b5678d6dcfb2524dfd8bf0abb23c671b
tomon9086 <tomon9086@gmail.com>
2021-06-07 16:51:42 +0900

    install material-ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,47 @@
+import { Box, Button } from '@material-ui/core'
 import icon from '@/../assets/icon.svg'
 import '@/App.global.css'
 
 const Home = () => {
   return (
-    <div>
+    <Box
+      alignItems='center'
+      display='flex'
+      flexDirection='column'
+      height='100%'
+      justifyContent='center'
+    >
       <div className='Hello'>
         <img alt='icon' src={icon} width='200px' />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <div className='Hello'>
+      <Box m={1}>
         <a
           href='https://electron-react-boilerplate.js.org/'
           rel='noreferrer'
           target='_blank'
         >
-          <button type='button'>
+          <Button variant='contained'>
             <span aria-label='books' role='img'>
               📚
             </span>
             Read our docs
-          </button>
+          </Button>
         </a>
         <a
           href='https://github.com/sponsors/electron-react-boilerplate'
           rel='noreferrer'
           target='_blank'
         >
-          <button type='button'>
+          <Button variant='contained'>
             <span aria-label='books' role='img'>
               🙏
             </span>
             Donate
-          </button>
+          </Button>
         </a>
-      </div>
-    </div>
+      </Box>
+    </Box>
   )
 }
 
